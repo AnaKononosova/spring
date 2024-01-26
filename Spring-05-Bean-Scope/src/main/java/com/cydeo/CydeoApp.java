86627a97a5cb58d36474a01d597c8bf8b7f2267f
Anastasia <anastasia.kononosova@ncino.com>
2024-01-26 12:20:05 -0700

    abstraction & bean scope

---
+++ Spring-05-Bean-Scope/src/main/java/com/cydeo/CydeoApp.java
@@ -0,0 +1,28 @@
+package com.cydeo;
+
+import com.cydeo.config.ProjectConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp
+{
+
+  public static void main(String[] args)
+  {
+    Comment comment = new Comment();
+    comment.setAuthor("Jonson");
+    comment.setText("Spring framework");
+
+    ApplicationContext container = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+    CommentService cs1 = container.getBean(CommentService.class);
+    CommentService cs2 = container.getBean(CommentService.class);
+
+    System.out.println(cs1);
+    System.out.println(cs2);
+    System.out.println(cs1==cs2);
+  }
+
+}

ca1e54483a916ba2a20ff29f7bdc38fc3697af89
Anastasia <anastasia.kononosova@ncino.com>
2024-02-05 13:42:55 -0700

    Spring mvc

---
+++ Spring-06-SpringBoot-Demo/src/test/java/com/cydeo/CydeoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cydeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CydeoApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
